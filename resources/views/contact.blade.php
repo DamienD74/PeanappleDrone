31cd22b5c43885e9dc9eb0a9d037de6b26349ac8
DamienD74 <damien.delhaye@le-campus-numerique.fr>
2021-02-24 15:53:26 +0100

    depart contact

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,6 +5,35 @@
     <title>Title</title>
 </head>
 <body>
+@extends('layout')
 
+<div class="fondCO">
+    <div>
+
+    </div>
+    <div>
+        <div class="textCO">
+            nom d'utilisateur ou email
+        </div>
+        <input type="text" id="name" name="name" required
+               minlength="10" maxlength="20" size="25">
+    </div>
+    <div>
+        <div class="textCO">
+            Mot de passe
+        </div>
+        <input type="text" id="mdp" name="mdp" required
+               minlength="10" maxlength="20" size="25">
+        <div>
+
+        </div>
+    </div>
+    <div>
+
+    </div>
+    <div class="textCO">
+        Nouvelle utilisateur ?
+    </div>
+</div>
 </body>
 </html>
