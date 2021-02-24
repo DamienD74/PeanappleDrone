7415759a441cf0da59f3f245987bee21ffc4f9de
NNACHID <nicolas.nachid@le-campus-numerique.fr>
2021-02-24 15:21:28 +0100

    Premier push et debut du panier

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -6,5 +6,52 @@
 </head>
 <body>
 
+
+<main>
+
+    <div class="cart_article">
+        <div>
+            <div class="article_image">
+                <img src="/* splash-drone-3----p-image-203855-grande-removebg-preview 2 */
+
+position: absolute;
+width: 150px;
+height: 89px;
+left: calc(50% - 150px/2 - 97.5px);
+top: 237px;
+
+background: url(splash-drone-3----p-image-203855-grande-removebg-preview.png);
+">
+            </div>
+            <div class="article_description">
+                <h2>Consectetur adipg elit.</h2>
+                <p>Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit.
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit.</p>
+            </div>
+        </div>
+        <div>
+            <div class="quantifier">
+
+            </div>
+            <div class="price">
+
+            </div>
+        </div>
+    </div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+    <div>
+
+    </div>
+
+</main>
+
+
 </body>
 </html>
