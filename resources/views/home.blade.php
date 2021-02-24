e053cb49a48b8174209b1fe30896fe49415753f6
Lesnier Lucie <lucie.lesnier@le-campus-numerique.fr>
2021-02-24 15:48:40 +0100

    style css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,22 +1,22 @@
 @extends('layout')
 @section('content')
-
-    <div class="imageHome"><img src=href={{asset('public/image_acceuil/FirthImage.png')}} alt="Image" width="1000" height="666"> <a class="titreHome">Phantom 4 Pro
+<main>
+    <div class="imageHome"><img src= {{asset('/image_acceuil/FirthImage.png')}} alt="Image" width="1000" height="666"> <a class="titreHome">Phantom 4 Pro
             V2 </a><p class="texte1">Caméra embarquée, détection et évitement d’obstacles, assistance en vol...</p></div>
-    <div class="imageHome"><img src="{{asset('public/image_acceuil//SecondImage.png')}}" alt="Image" width="1000" height="666"> <a class="titreHome">Potensic T25
+    <div class="imageHome"><img src={{asset('/image_acceuil//SecondImage.png')}} alt="Image" width="1000" height="666"> <a class="titreHome">Potensic T25
             <p class="texteHome">GPS intégré, caméra HD, mode “Return home”, adapté aux débutants... </p></a>
     </div>
-    <div class="imageHome"><img src="{{asset('/public/image_acceuil/ThirdImage.png')}} alt="Image" width="1000" height="666"> <a class="titreHome">Agras MG-1S RTK
+    <div class="imageHome"><img src={{asset('/image_acceuil/ThirdImage.png')}} alt="Image" width="1000" height="666"> <a class="titreHome">Agras MG-1S RTK
             <p class="texteHome"> 10 l de cargaison, autonomie jusqu’à 22 minutes, traitement centimétrique...</p></a>
     </div>
-    <div class="imageHome"><img src="{{asset('public/image_acceuil/FourthImage.png')}} alt="Image" width="1000" height="666"> <a class="titreHome">Cheerson CX-35
+    <div class="imageHome"><img src={{asset('/image_acceuil/FourthImage.png')}} alt="Image" width="1000" height="666"> <a class="titreHome">Cheerson CX-35
             Pro <p class="texteHome">10 l de cargaison, autonomie jusqu’à 22 minutes, traitement centimétrique...</p></a>
     </div>
-    <div class="imageHome"><img src="{{asset('public/image_acceuil/FirthImage.png')}} alt="Image" width="1000" height="666"> <a class="titreHome">Spark- Emotion
+    <div class="imageHome"><img src={{asset('/image_acceuil/FirthImage.png')}} alt="Image" width="1000" height="666"> <a class="titreHome">Spark- Emotion
             instantanée <p class="texteHome">Options intelligentes de contrôles, caméra HD, adapté aux débutants...</p></a>
     </div>
-
+</main>
 
 @stop
 
-@extends('footer')
+
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -1,7 +1,10 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
- <link rel="stylesheet"  href="{{ asset('css/style.css')}}">)
+    <link rel="preconnect" href="https://fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300&display=swap" rel="stylesheet">
+ <link rel="stylesheet"  href="{{ asset('css/style.css')}}">
+
     <meta charset="UTF-8">
     <title>@yield('Titre')</title>
 </head>
