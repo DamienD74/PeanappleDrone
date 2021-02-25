1cf88b42bff4fc6c91f8c3ba836ec927df8a3e57
DamienD74 <damien.delhaye@le-campus-numerique.fr>
2021-02-25 10:13:21 +0100

    Contact 0.3

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,16 +3,16 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('Titre')</title>
-    <link rel="stylesheet"  href="{{ asset('css/contact.css')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
+
     <link rel="stylesheet"  href="{{ asset('css/contact.css')}}">
 </head>
 <body>
 
-
 @yield('content')
-@extends('footer')
-
 
 </body>
+<footer>
+    @yield('footer')
+</footer>
 </html>
