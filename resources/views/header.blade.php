2c31e5d88e25cc24f532fe900a95d2264fc98565
Lesnier Lucie <lucie.lesnier@le-campus-numerique.fr>
2021-02-25 11:49:02 +0100

    Header strucure

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -1,21 +1,21 @@
-<div class="header">
-<h1>Pineapple Drone</h1>
-<nav class="navbar navbar-light bg-light">
-    <form class="form-inline">
-        <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
-</nav>
-<div class="dropdown">
-    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenu2" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-        Dropdown
-    </button>
-    <div class="dropdown-menu" aria-labelledby="dropdownMenu2">
-        <button class="dropdown-item" type="button">Action</button>
-        <button class="dropdown-item" type="button">Another action</button>
-        <button class="dropdown-item" type="button">Something else here</button>
+
+<head>
+
+    <link rel="stylesheet"  href="{{ asset('css/header.css')}}">
+
+</head>
+<header>
+    <div>
+        <a></a>
+        <a></a>
+        <a></a>
+        <a></a>
     </div>
-</div>
-</div>
+    <div>
 
-
+    </div>
+    <div>
+        <a></a>
+        <a></a>
+    </div>
+</header>
--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,15 +3,15 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('title')</title>
-
+    <link rel="stylesheet"  href="{{ asset('css/style.css')}}">
     <link rel="stylesheet"  href="{{ asset('css/home.css')}}">
-    <link rel="stylesheet"  href="{{ asset('css/header.css')}}">
+
     <meta name="viewport" content="width=device-width, initial-scale=1">
     @yield('style')
 </head>
 <body>
 
-
+@include('header')
 @yield('content')
 
 <footer>
