08ae958086da378743cd345af39c941dc996da34
Lesnier Lucie <lucie.lesnier@le-campus-numerique.fr>
2021-02-25 11:09:02 +0100

    Home OK, CSS home OK, header in progress

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>@yield('Titre')</title>
-    <link rel="stylesheet"  href="{{ asset('css/contact.css')}}">
+
     <link rel="stylesheet"  href="{{ asset('css/home.css')}}">
     <link rel="stylesheet"  href="{{ asset('css/header.css')}}">
     <meta name="viewport" content="width=device-width, initial-scale=1">
