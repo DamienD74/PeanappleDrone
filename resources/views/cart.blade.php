18e3928c80f8b3e058eaa29fa0012735d7a56e5c
NNACHID <nicolas.nachid@le-campus-numerique.fr>
2021-02-25 16:14:31 +0100

    commit de fin de journée, ajout des dernieres modif du panier(bientot terminer)

--- resources/views/cart.blade.php
+++ resources/views/cart.blade.php
@@ -30,7 +30,40 @@ Panier
             </div>
             <div class="price">
 
-                <h1>85787544</h1>
+                <h1>Prix :</h1>
+                <h2>1'559.00 $</h2>
+            </div>
+        </div>
+    </div>
+    <div class="cart_article">
+
+        <div class="cart_content">
+            <div class="article_image">
+                <img alt="" src="{{asset('img/drone/splash-drone-3----p-image-203855-grande-removebg-preview.png')}}"/>
+            </div>
+            <div class="article_description">
+                <h2>Consectetur adipg elit.</h2>
+                <p>Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit.
+                    Lorem ipsum dolor sit amet,
+                    consectetur adipiscing elit.</p>
+            </div>
+
+        </div>
+        <div class="cart_content">
+            <div class="quantifier_container">
+                <div class="quantifier">
+
+                <button></button><input type="text"><button></button>
+
+
+
+                </div>
+            </div>
+            <div class="price">
+
+                <h1>Prix :</h1>
+                <h2>1'559.00 $</h2>
             </div>
         </div>
     </div>
@@ -58,35 +91,8 @@ Panier
             </div>
             <div class="price">
 
-                <h1>85787544</h1>
-            </div>
-        </div>
-    </div>
-    <div class="cart_article">
-
-        <div class="cart_content">
-            <div class="article_image">
-                <img alt="" src="{{asset('img/drone/splash-drone-3----p-image-203855-grande-removebg-preview.png')}}"/>
-            </div>
-            <div class="article_description">
-                <h2>Consectetur adipg elit.</h2>
-                <p>Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit.
-                    Lorem ipsum dolor sit amet,
-                    consectetur adipiscing elit.</p>
-            </div>
-
-        </div>
-        <div class="cart_content">
-            <div class="quantifier">
-
-
-                <h1>754552554</h1>
-
-            </div>
-            <div class="price">
-
-                <h1>85787544</h1>
+                <h1>Prix :</h1>
+                <h2>1'559.00 $</h2>
             </div>
         </div>
     </div>
