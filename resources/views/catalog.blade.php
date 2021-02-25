55f79cf639967703b118aa0f30925a554276f0c2
Arthur Marichal <arthur.marichal@le-campus-numerique.fr>
2021-02-25 15:33:27 +0100

    Nouvelles modif catalogue (hteml/css)

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -6,7 +6,7 @@
     <div class="RealbgCat">
     <div class="BgDivCat">
         <div>
-            <img class="Catimg" src="{{asset('/img/drone/splash-drone-3----p-image-203855-grande-removebg-preview.png')}}" alt="Splash Drone" >
+            <img class="Catimg"  src="{{asset('/img/drone/splash-drone-3----p-image-203855-grande-removebg-preview.png')}}" alt="Splash Drone" ><a href="ficheProduit.blade.php"></a>
             <h3 class="textCatH3">Helsel Splash Drone 3+</h3>
             <p class="textCat">
                 Caméra : Non fournis<br>
@@ -20,7 +20,7 @@
     </div>
     <div class="BgDivCat">
         <div>
-            <img class="Catimg" src="{{asset('/img/drone/aile-volante-fpv-sonic-mo-ar_wing_pnp-removebg-preview.png')}}" alt="Splash Drone" height="200rem" width="300rem">
+            <img class="Catimg" src="{{asset('/img/drone/aile-volante-fpv-sonic-mo-ar_wing_pnp-removebg-preview.png')}}" alt="Splash Drone"><a href="ficheProduit.blade.php"></a>
             <h3 class="textCatH3">Helsel Splash Drone 3+</h3>
             <p class="textCat">
                 Caméra : Non fournis<br>
@@ -34,7 +34,7 @@
     </div>
     <div class="BgDivCat">
         <div>
-            <img class="Catimg" src="{{asset('/img/drone/beta95x-v3-hd-bnf---betafpv-p-image-222473-grande-removebg-preview.png')}}" alt="Splash Drone">
+            <img class="Catimg" src="{{asset('/img/drone/beta95x-v3-hd-bnf---betafpv-p-image-222473-grande-removebg-preview.png')}}" alt="Splash Drone"><a href="ficheProduit.blade.php"></a>
             <h3 class="textCatH3">Helsel Splash Drone 3+</h3>
             <p class="textCat">
                 Caméra : Non fournis<br>
@@ -48,7 +48,7 @@
     </div>
     <div class="BgDivCat">
         <div>
-            <img class="Catimg" src="{{asset('/img/drone/drone-r-falcon-camera-planification-vol-direction-lock-e1504889086161-removebg-preview.png')}}" alt="Splash Drone">
+            <img class="Catimg" src="{{asset('/img/drone/drone-r-falcon-camera-planification-vol-direction-lock-e1504889086161-removebg-preview.png')}}" alt="Splash Drone"><a href="ficheProduit.blade.php"></a>
             <h3 class="textCatH3">Helsel Splash Drone 3+</h3>
             <p class="textCat">
                 Caméra : Non fournis<br>
@@ -62,7 +62,7 @@
     </div>
     <div class="BgDivCat">
         <div>
-            <img class="Catimg" src="{{asset('/img/drone/parrot_mambo_fly_s1802234396430A_174730244-removebg-preview.png')}}" alt="Splash Drone">
+            <img class="Catimg" src="{{asset('/img/drone/parrot_mambo_fly_s1802234396430A_174730244-removebg-preview.png')}}" alt="Splash Drone"><a href="ficheProduit.blade.php"></a>
             <h3 class="textCatH3">Helsel Splash Drone 3+</h3>
             <p class="textCat">
                 Caméra : Non fournis<br>
