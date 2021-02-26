e7e794af7619f1328223f4b6ed43097da55c45fc
DamienD74 <damien.delhaye@le-campus-numerique.fr>
2021-02-26 09:09:34 +0100

    resolve lien

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -5,11 +5,10 @@ namespace App\Http\Controllers;
 
 use Illuminate\Routing\Controller as BaseController;
 
-class HomeController extends BaseController {
+class HomeController extends BaseController
+{
     function home(): string
     {
         return view('home');
     }
-}
-
 }
