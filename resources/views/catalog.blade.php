70f6a7dcd91e9e4458b996c5cae49b606c828e67
Arthur Marichal <arthur.marichal@le-campus-numerique.fr>
2021-02-26 09:33:13 +0100

    Catalogue responsive @media query

--- resources/views/catalog.blade.php
+++ resources/views/catalog.blade.php
@@ -74,5 +74,47 @@
             <p class="textCat CatnoMarg">1559.00€</p>
         </div>
     </div>
+        <div class="BgDivCat">
+            <div>
+                <img class="Catimg" src="{{asset('/img/drone/beta95x-v3-hd-bnf---betafpv-p-image-222473-grande-removebg-preview.png')}}" alt="Splash Drone"><a href="ficheProduit.blade.php"></a>
+                <h3 class="textCatH3">Helsel Splash Drone 3+</h3>
+                <p class="textCat">
+                    Caméra : Non fournis<br>
+                    Poids : 1447g<br>
+                    Temps de vol : 20 minutes<br>
+                    Vitesse en vol max : 20m/s
+                </p>
+                <div class="Catlign"></div>
+                <p class="textCat">1559.00€</p>
+            </div>
+        </div>
+        <div class="BgDivCat">
+            <div>
+                <img class="Catimg" src="{{asset('/img/drone/drone-r-falcon-camera-planification-vol-direction-lock-e1504889086161-removebg-preview.png')}}" alt="Splash Drone"><a href="ficheProduit.blade.php"></a>
+                <h3 class="textCatH3">Helsel Splash Drone 3+</h3>
+                <p class="textCat">
+                    Caméra : Non fournis<br>
+                    Poids : 1447g<br>
+                    Temps de vol : 20 minutes<br>
+                    Vitesse en vol max : 20m/s
+                </p>
+                <div class="Catlign"></div>
+                <p class="textCat">1559.00€</p>
+            </div>
+        </div>
+        <div class="BgDivCat">
+            <div>
+                <img class="Catimg" src="{{asset('/img/drone/parrot_mambo_fly_s1802234396430A_174730244-removebg-preview.png')}}" alt="Splash Drone"><a href="ficheProduit.blade.php"></a>
+                <h3 class="textCatH3">Helsel Splash Drone 3+</h3>
+                <p class="textCat">
+                    Caméra : Non fournis<br>
+                    Poids : 1447g<br>
+                    Temps de vol : 20 minutes<br>
+                    Vitesse en vol max : 20m/s
+                </p>
+                <div class="Catlign"></div>
+                <p class="textCat CatnoMarg">1559.00€</p>
+            </div>
+        </div>
     </div>
 @endsection
