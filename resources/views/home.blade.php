78d0a7001b3f18eee399b3b58b794df97e85f44d
Lesnier Lucie <lucie.lesnier@le-campus-numerique.fr>
2021-02-26 11:38:17 +0100

    route OK, redirecting OK

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,24 +3,24 @@
 <div class="blockHome">
 
     <div class="imageHome" id="imgHome1">
-        <div class="titreHome"> <a href="products.blade.php"> <span style="color: white; "> Phantom V7 </span></a> </div>
+        <div class="titreHome"> <a href="{{route('product')}}"> <span style="color: white; "> Phantom V7 </span></a> </div>
         <div class="textHome">Lorem</div>
     </div>
     <div class="imageHome" id="imgHome2">
-        <div class="titreHome"><a href="products.blade.php"><span style="color: white; "> Phantom V7 </span></a> </div>
+        <div class="titreHome"><a href="{{route('product')}}"><span style="color: white; "> Phantom V7 </span></a> </div>
         <div class="textHome">Lorem</div>
     </div>
     <div class="imageHome" id="imgHome3">
-        <div class="titreHome"> <a href="products.blade.php"><span style="color: white; "> Phantom V7 </span></a> </div>
+        <div class="titreHome"> <a href="{{route('product')}}"><span style="color: white; "> Phantom V7 </span></a> </div>
         <div class="textHome">Lorem</div>
 
     </div>
     <div class="imageHome" id="imgHome4">
-        <div class="titreHome"> <a href="products.blade.php"> <span style="color: white; ">Phantom V7</span></a></div>
+        <div class="titreHome"> <a href="{{route('product')}}"> <span style="color: white; ">Phantom V7</span></a></div>
         <div class="textHome">Lorem</div>
     </div>
     <div class="imageHome" id="imgHome5">
-        <div class="titreHome"> <a href="products.blade.php"> <span style="color: white; ">Phantom V7</span></a></div>
+        <div class="titreHome"> <a href="{{route('product')}}"> <span style="color: white; ">Phantom V7</span></a></div>
         <div class="textHome">Lorem</div>
     </div>
 </div>
